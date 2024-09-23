674f192f371d57411016177a4628902b623d6924
Felipe <www.felipecesargomes@gmail.com>
2024-09-23 20:57:38 -0300

    test:user resource

--- src/main/java/com/felipe/rest/PostResource.java
+++ src/main/java/com/felipe/rest/PostResource.java
@@ -4,6 +4,7 @@ import com.felipe.domain.Post;
 import com.felipe.domain.User;
 import com.felipe.dto.CreatePostRequest;
 import com.felipe.dto.PostResponse;
+import com.felipe.repository.FollowerRepository;
 import com.felipe.repository.PostRepository;
 import com.felipe.repository.UserRepository;
 import io.quarkus.hibernate.orm.panache.PanacheQuery;
@@ -25,11 +26,13 @@ public class PostResource {
 
     private UserRepository userRepository;
     private PostRepository repository;
+    private final FollowerRepository followerRepository;
 
     @Inject
-    public PostResource(UserRepository userRepository, PostRepository repository) {
+    public PostResource(UserRepository userRepository, PostRepository repository, FollowerRepository followerRepository) {
         this.userRepository = userRepository;
         this.repository = repository;
+        this.followerRepository = followerRepository;
     }
 
     @POST
@@ -48,11 +51,31 @@ public class PostResource {
     }
 
     @GET
-    public Response listPost(@PathParam("userId") Long userId) {
+    public Response listPost(@PathParam("userId") Long userId, @HeaderParam("followerId") Long followerId) {
         User user = userRepository.findById(userId);
         if(user == null) {
             return Response.status(Response.Status.NOT_FOUND).build();
         }
+
+        if(followerId == null) {
+            return Response.status(Response.Status.BAD_REQUEST)
+                    .entity("Você esqueceu o cabeçalho")
+                    .build();
+        }
+
+        User follower = userRepository.findById(followerId);
+
+        if(follower == null) {
+            return Response.status(Response.Status.BAD_REQUEST)
+                    .entity("Não existe followerId informado")
+                    .build();
+        }
+
+        boolean follows = followerRepository.followes(follower, user);
+        if(!follows) {
+            return Response.status(Response.Status.FORBIDDEN).entity("Você não consegue ver os posts").build();
+        }
+
         PanacheQuery<Post> query = repository.find("user", Sort.by("dateTime", Sort.Direction.Descending), user);
         var list = query.list();
         List<PostResponse> collect = list.stream().map(post -> PostResponse.fromEntity(post)).collect(Collectors.toList());
---
+++ src/test/java/com/felipe/rest/UserResourceTest.java
@@ -0,0 +1,35 @@
+package com.felipe.rest;
+
+import com.felipe.dto.CreateUserRequest;
+import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.jupiter.api.Assertions.*;
+
+@QuarkusTest
+class UserResourceTest {
+
+    @Test
+    @DisplayName("deve criar um usuário com sucesso!")
+    public void createUserTest() {
+        var user = new CreateUserRequest();
+        user.setName("teste usuario");
+        user.setAge(30);
+
+        Response response = given()
+                .contentType(ContentType.JSON).body(user)
+                .when()
+                .post("/users")
+                .then()
+                .extract().response();
+
+        assertEquals(201, response.statusCode());
+        assertNotNull(response.jsonPath().getString("id"));
+
+    }
+
+}
