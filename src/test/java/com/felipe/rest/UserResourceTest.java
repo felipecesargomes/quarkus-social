57e4475138adc366da57bb0ef911f127bb226578
Felipe <www.felipecesargomes@gmail.com>
2024-09-23 22:53:36 -0300

    test:Error Test User Resource

--- src/test/java/com/felipe/rest/UserResourceTest.java
+++ src/test/java/com/felipe/rest/UserResourceTest.java
@@ -1,6 +1,7 @@
 package com.felipe.rest;
 
 import com.felipe.dto.CreateUserRequest;
+import com.felipe.dto.ResponseError;
 import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
@@ -32,10 +33,24 @@ class UserResourceTest {
 
     }
 
-  /**  @Test
-    @DisplayName("")
+    @Test
+    @DisplayName("Deve retornar erro quando json não é válido")
     public void createUserValidationErrorTest() {
+        var user = new CreateUserRequest();
+        user.setAge(null);
+        user.setName(null);
 
-    }**/
+        Response response = given()
+                .contentType(ContentType.JSON)
+                .body(user)
+                .when()
+                .post("/users")
+                .then()
+                .extract().response();
+
+        assertEquals(ResponseError.UNPROCESSABLE_ENTITY_STATUS, response.statusCode());
+        assertEquals("Validation Error", response.jsonPath().getString("message"));
+
+    }
 
 }
